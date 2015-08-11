e179f82f82d6cb6dde12e9aa2baf070a2a4b6453
Trismegiste <flo@trismegiste.net>
2015-08-12 00:08:30 +0200

    combat symetrical ok

--- js/simul.js
+++ js/simul.js
@@ -145,16 +145,23 @@ var Combat = {
 
         return result;
     }
-}
+};
 
 var pj = new Character();
 var pnj = new Character();
 
-var stat = [];
-//for (var k = 0; k < 10; k++) {
-//    var res = Combat.getResult(pj, pnj);
-//    stat[res]++;
-//}
+var stat = [0, 0, 0, 0];
+for (var k = 0; k < 10000; k++) {
+    var res = Combat.getResult(pj, pnj);
+    stat[res]++;
+    res = Combat.getResult(pnj, pj);
+    if (res === 2) {
+        res = 1
+    } else if (res === 1) {
+        res = 2
+    }
+    stat[res]++;
+}
 
 
 console.log(stat)
