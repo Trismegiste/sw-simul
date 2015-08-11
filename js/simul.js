f84856c04c197d8fcd7d847ebee51683f76120fe
Trismegiste <flo@trismegiste.net>
2015-08-12 01:28:23 +0200

    simul ok

--- js/simul.js
+++ js/simul.js
@@ -1,9 +1,33 @@
-var pj = new Character();
-pj.combatVal=4;
-var pnj = new Character();
+var model = [
+    new Character(),
+    new Character(),
+    new Character(),
+    new Character()
+];
 
-var stat = Combat.runSimul(pj, pnj, 10000);
+model.forEach(function (charact, k) {
+    $('div[data-model-pk=' + k + '] input[name=vigor]').val(charact.vigor);
+    $('div[data-model-pk=' + k + '] input[name=armour]').val(charact.armour);
+    $('div[data-model-pk=' + k + '] input[name=combat]').val(charact.combatVal);
+});
 
-console.log(stat)
+$('#pulsa-start').click(function (event) {
+    event.stopPropagation();
+    event.preventDefault();
 
+    model.forEach(function (charact, k) {
+        charact.vigor = parseInt($('div[data-model-pk=' + k + '] input[name=vigor]').val());
+        charact.armour = parseInt($('div[data-model-pk=' + k + '] input[name=armour]').val());
+        charact.combatVal = parseInt($('div[data-model-pk=' + k + '] input[name=combat]').val());
+    });
 
+    model.forEach(function (pnj, k) {
+        if (k > 0) {
+            var result = Combat.runSimul(model[0], pnj, 10000);
+            $('div[data-model-pk=' + k + '] .result .victory').text(result[1]);
+            $('div[data-model-pk=' + k + '] .result .defeat').text(result[2]);
+            $('div[data-model-pk=' + k + '] .result .draw').text(result[3]);
+        }
+    });
+
+});
